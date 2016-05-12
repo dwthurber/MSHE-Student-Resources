5493f56cbc347a1ecb7a7a27a36f4fa1a27f54ed
Derek T <fbfb1694@opayq.com>
2016-05-12 22:52:36 +0000

    updated to database app

--- js/app.js
+++ js/app.js
@@ -3,14 +3,22 @@ var topicsApp = angular.module('topicsApp', ['ngRoute']);
     // create the controller and inject Angular's $scope
     topicsApp.controller('mainController', function($scope, $route, $routeParams, $location, $http) {
         
-        $http({
-          method: 'GET',
-          url: './master_projects.json'
-        }).then(function successCallback(response) {
-            $scope.masterProjects = response.data;
-          }, function errorCallback(response) {
-            console.log(response.status);
-          });
+        // $http({
+        //   method: 'GET',
+        //   url: './master_projects.json'
+        // }).then(function successCallback(response) {
+        //     $scope.masterProjects = response.data;
+        //   }, function errorCallback(response) {
+        //     console.log(response.status);
+        //   });
+        
+        // var getTags = function(data) {  
+        //     angular.foreach($scope.masterProjects, function(tags) {
+        //         $scope.tags = tags.tags;
+                
+        //     });
+        //     console.log($scope.tags);
+        // };
 
         $scope.filterFilter = 'category';
         
@@ -35,29 +43,38 @@ var topicsApp = angular.module('topicsApp', ['ngRoute']);
          $scope.sort = function(val) {
              $scope.sortOrder = val;
              $scope.reverse = ($scope.sortOrder === val) ? !$scope.reverse : false;
+             
+             if ($scope.reverse === true) {
+                 var ascdesc = 'desc'
+             } else {
+                 var ascdesc = 'asc'
+             }
+             
+             $('#Container').mixItUp('sort', val + ':' + ascdesc);
          };
 
-        $scope.categoryIncludes = [];
+        // // $scope.categoryIncludes = [];
 
-        $scope.filterCat = function(newVal) {
-            var i = $.inArray(newVal, $scope.categoryIncludes);
-            if (i > -1) {
-                $scope.categoryIncludes.splice(i, 1);
-            } else {
-                $scope.categoryIncludes.push(newVal);
-            }
+        // // $scope.filterCat = function(newVal) {
+        // //     var i = $.inArray(newVal, $scope.categoryIncludes);
+        // //     if (i > -1) {
+        // //         $scope.categoryIncludes.splice(i, 1);
+        // //     } else {
+        // //         $scope.categoryIncludes.push(newVal);
+        // //     }
             
-            console.log($scope.categoryIncludes);
-        };
+        // //     console.log($scope.categoryIncludes);
+        // // };
         
-        $scope.filterByCategory = function(mp) {
-            if ($scope.categoryIncludes.length > 0) {
-                if ($.inArray(mp.categories, $scope.categoryIncludes) < 0)
-                    return;
-            }
-            
-            return mp;
-        }
+        // // $scope.filterByCategory = function(mp) {
+        // //     if ($scope.categoryIncludes.length > 0) {
+        // //         angular.foreach($scope.categoryIncludes, function(key) {
+        // //             if ($scope.masterProjects.master_projects.categories.indexof(key) >= 0) 
+        // //                 return;
+        // //         })
+        // //     }
+        // //     return mp;
+        // // }
          
          $(function(){
 
@@ -66,8 +83,8 @@ var topicsApp = angular.module('topicsApp', ['ngRoute']);
     	    $('#Container').mixItUp({
             	load: {
             		filter: mixitup,
-            		sort: 'name:asc',
-            	}
+            		sort: 'year:desc name:asc',
+            	}, 
             });
     
         });
